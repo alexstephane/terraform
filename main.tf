333c747aacffe0ac693a750f640a1138d396ca0d
alexngundji@gmail.com <alexngundji@gmail.com>
2023-01-14 15:58:54 -0600

    add

--- main.tf
+++ main.tf
@@ -10,3 +10,26 @@ resource "aws_instance" "example" {
     Name = "terraform-example"
   }
 }
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03ebea40da8b094a0"
+  
+
+   user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags ={
+    Name = "terraform-example"
+  }
+}
