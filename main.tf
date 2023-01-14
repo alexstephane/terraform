09a48e856f4e1d25bec21e81e39faafec5b3d503
alexngundji@gmail.com <alexngundji@gmail.com>
2023-01-14 16:02:30 -0600

    update

--- main.tf
+++ main.tf
@@ -6,19 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   subnet_id = "subnet-03ebea40da8b094a0"
-  tags ={
-    Name = "terraform-example"
-  }
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-03ebea40da8b094a0"
   
 
    user_data = <<-EOF
